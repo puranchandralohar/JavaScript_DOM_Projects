d3478fa7a0f6238918d9a282c85d0a20a71e4a32
puranchandralohar <clpuran95@gmail.com>
2022-08-17 01:10:49 +0530

    ready

--- sidebar/app.js
+++ sidebar/app.js
@@ -1,7 +1,17 @@
 let button  = document.querySelector('.sidebar-toggle');
 
+let sidebar = document.querySelector('.sidebar');
 
-button.addEventListener('click',()=>{
 
-    button.innerHTML  = `<i class="fa-solid fa-xmark"></i>`;
-})
+button.addEventListener('click',(e)=>{
+    if(e.target.className.includes('fa-bars')){
+        button.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+        sidebar.style.width = "300px";
+        sidebar.style.padding = "20px";
+    }
+    else if(e.target.className.includes('fa-xmark')){
+        button.innerHTML = '<i class="fa-solid fa-bars"></i>'
+        sidebar.style.width = "0px";
+        sidebar.style.padding = "0px";
+    }
+});
